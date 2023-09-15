ff2d5cccb89b349300ea252c4eae3a74f26c0e36
Marco Maschauer <68637879+marcomaschauer@users.noreply.github.com>
2023-09-15 16:45:47 +0200

    Improvements And Cleanup
    1. **Directory for AUR Packages**:
       - Defined a directory (`~/.aur-helper`) where all the cloned repositories for AUR packages would reside.
    
    2. **Debug Mode**:
       - Introduced a `DEBUG` flag which is activated with the `--debug` argument.
       - Modified how commands run depending on the `DEBUG` flag.
       - Removed `--debug` from the `sys.argv` list to prevent it from interfering with other arguments.
    
    3. **Better Feedback**:
       - Enhanced the feedback provided to users. The script now prints the status and results of each operation.
    
    4. **Error Handling**:
       - Improved error handling by checking the return codes of commands.
       - Errors are printed out to the user regardless of whether the `DEBUG` flag is set.
    
    5. **Rollback Functionality**:
       - Added a mechanism to undo actions during the installation if any step fails.
       - Created a function `rollback_installation` that will handle the rollback if necessary, primarily by deleting the cloned repository.
    
    6. **Improved Update Check**:
       - Modified the update function to check if the repository has new changes using `git pull`. If no changes are found, the script won't attempt to reinstall the package.
       - If no packages need updates, it prints "Nothing to do."
    
    7. **Package Removal Enhancement**:
       - When a package is removed using the `remove` command, its associated directory in `~/.aur-helper` is also removed.
    
    8. **Improved Error Messages**:
       - Standardized error messages to start with `==> ERROR:` for clarity.
       - The actual error message from the command execution is displayed to the user.
    
    9. **Code Cleanup & Refactoring**:
       - Several utility functions, such as `run_cmd` and `run_cmd_with_output`, were created or modified to improve code readability and structure.
    
    These changes collectively made the script more robust, user-friendly, and informative. The error-handling improvements, in particular, would significantly help in avoiding unintended consequences during the installation or updating of AUR packages.

---
+++ README.md
@@ -0,0 +1,49 @@
+## AUR Helper
+
+A simple tool to manage AUR packages on Arch Linux.
+
+### Features
+
+- **Installation**: Easily install packages directly from git repositories.
+- **Update**: Update all installed AUR packages.
+- **Removal**: Remove installed AUR packages and their associated data.
+
+### Usage
+
+1. **Installing AUR Packages**:
+
+   Use the `install` command followed by the link to the git repository.
+   
+   ```bash
+   aur-helper install [link-to-git-repository]
+   ```
+   
+   In case of errors during the installation, the tool will automatically rollback any changes made during the installation process and provide feedback.
+
+2. **Updating AUR Packages**:
+
+   Simply use the `update` command to update all the installed AUR packages.
+   
+   ```bash
+   aur-helper update
+   ```
+
+3. **Removing AUR Packages**:
+
+   Use the `remove` command followed by the package name you wish to remove.
+   
+   ```bash
+   aur-helper remove [packagename]
+   ```
+
+   This will also remove the associated package data from `~/.aur-helper`.
+
+### Debugging
+
+For detailed output, use the `--debug` argument. This provides verbose information which can be useful for troubleshooting.
+
+```bash
+aur-helper --debug [command] [arguments]
+```
+
+---
--- aur-helper.py
+++ aur-helper.py
@@ -1,61 +1,117 @@
 #!/usr/bin/env python3
 
-import sys
 import os
+import sys
+import shutil
 import subprocess
 
 AUR_DIR = os.path.expanduser("~/.aur-helper")
 
+DEBUG = "--debug" in sys.argv
+
 def run_cmd(cmd):
+    if DEBUG:
+        print(f"Executing: {' '.join(cmd)}\n")
+        process = subprocess.Popen(cmd)
+    else:
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    _, stderr = process.communicate()
+
+    if process.returncode != 0:
+        return stderr.decode() if stderr else 'Unknown error.'
+    return None
+
+def run_cmd_with_output(cmd):
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     return process.returncode, stdout.decode(), stderr.decode()
 
 def install_package(git_url):
+    package_name = git_url.split("/")[-1].replace(".git", "")
+    print(f"Installing package: {package_name}")
+
     os.makedirs(AUR_DIR, exist_ok=True)
-    
-    repo_name = os.path.basename(git_url).replace('.git', '')
-    clone_path = os.path.join(AUR_DIR, repo_name)
+    os.chdir(AUR_DIR)
 
-    # Clone the repo
-    returncode, stdout, stderr = run_cmd(["git", "clone", git_url, clone_path])
-    if returncode != 0:
-        print(stderr)
-        return
+    error = run_cmd(["git", "clone", git_url])
+    if error:
+        print(f"==> ERROR: Could not clone {package_name}. Message: {error}")
+        print("Rolling back installation steps...")
+        rollback_installation(package_name)
+        sys.exit(1)
 
-    # Build and install the package
-    os.chdir(clone_path)
-    returncode, stdout, stderr = run_cmd(["makepkg", "-si", "--noconfirm"])
-    if returncode != 0:
-        print(stderr)
+    os.chdir(package_name)
+    error = run_cmd(["makepkg", "-si", "--noconfirm"])
+    if error:
+        print(f"==> ERROR: Could not install {package_name}. Message: {error}")
+        print("Rolling back installation steps...")
+        rollback_installation(package_name)
+        sys.exit(1)
+
+    print(f"Package {package_name} was successfully installed!")
+
+def rollback_installation(package_name):
+    package_path = os.path.join(AUR_DIR, package_name)
+    if os.path.exists(package_path):
+        print(f"Removing cloned directory: {package_path}")
+        shutil.rmtree(package_path)
+    else:
+        print("No cloned directory found. Nothing to rollback.")
+
+def remove_package(package_name):
+    print(f"Attempting to remove package: {package_name}")
+
+    error = run_cmd(["sudo", "pacman", "-Rns", package_name, "--noconfirm"])
+    if error:
+        print(f"==> ERROR: Could not remove {package_name}. Message: {error}")
+        sys.exit(1)
+
+    # Remove the directory from ~/.aur-helper
+    package_dir = os.path.join(AUR_DIR, package_name)
+    if os.path.exists(package_dir):
+        print(f"Removing directory: {package_dir}")
+        shutil.rmtree(package_dir)
+
+    print(f"Package {package_name} was successfully removed!")
 
 def update_packages():
+    no_updates = True  # Initially assume no packages need updates
+
     for item in os.listdir(AUR_DIR):
         path = os.path.join(AUR_DIR, item)
         if os.path.isdir(path):
+            print(f"Checking updates for: {item}")
             os.chdir(path)
-            run_cmd(["git", "pull"])
-            returncode, stdout, stderr = run_cmd(["makepkg", "-si", "--noconfirm"])
-            if returncode != 0:
-                print(stderr)
 
-def remove_package(package_name):
-    returncode, stdout, stderr = run_cmd(["sudo", "pacman", "-Rns", package_name, "--noconfirm"])
-    if returncode != 0:
-        print(stderr)
+            _, stdout, _ = run_cmd_with_output(["git", "pull"])
+
+            if not "Already up to date." in stdout:
+                no_updates = False  # An update was found for at least one package
+                error = run_cmd(["makepkg", "-si", "--noconfirm"])
+                if error:
+                    print(f"==> ERROR: Could not update {item}. Message: {error}")
+                    sys.exit(1)
+
+    if no_updates:
+        print("Nothing to do.")
 
 if __name__ == "__main__":
+    if DEBUG:  # If --debug is present, remove it from sys.argv for further processing
+        sys.argv.remove("--debug")
+
     if len(sys.argv) < 2:
-        print("Usage: aur-helper (install|update|remove) [arg]")
+        print("Usage: aur-helper [install|remove|update] <args>")
         sys.exit(1)
 
     action = sys.argv[1]
-    
-    if action == "install" and len(sys.argv) == 3:
+
+    if action == "install" and len(sys.argv) > 2:
         install_package(sys.argv[2])
+    elif action == "remove" and len(sys.argv) > 2:
+        remove_package(sys.argv[2])
     elif action == "update":
         update_packages()
-    elif action == "remove" and len(sys.argv) == 3:
-        remove_package(sys.argv[2])
     else:
-        print("Invalid arguments!")
+        print("Invalid command or missing arguments")
+        sys.exit(1)
+
